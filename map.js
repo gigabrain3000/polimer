119d9bc804b7e0564795e7f083267ad1947f227a
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2025-05-29 11:40:59 +0300

    feat: add marker to map

--- map.js
+++ map.js
@@ -2,28 +2,44 @@ export {initMap};
 
 
 async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-    await ymaps3.ready;
+  // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+  await ymaps3.ready;
 
-    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+  const {YMap, YMapDefaultSchemeLayer} = ymaps3;
 
-    // Иницилиазируем карту
-    const map = new YMap(
-        // Передаём ссылку на HTMLElement контейнера
-        document.querySelector('.map'),
+  // Иницилиазируем карту
+  const map = new YMap(
+    // Передаём ссылку на HTMLElement контейнера
+    document.querySelector('.map'),
 
-        // Передаём параметры инициализации карты
-        {
-            location: {
-                // Координаты центра карты
-                center: [37.588144, 55.733842],
+    // Передаём параметры инициализации карты
+    {
+      location: {
+          // Координаты центра карты
+          center: [53.885437, 30.280879],
 
-                // Уровень масштабирования
-                zoom: 10
-            }
-        }
-    );
+          // Уровень масштабирования
+          zoom: 10
+      }
+    }
+  );
 
-    // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
+  const markerElement = document.createElement('div');
+  markerElement.className = 'marker-class';
+  markerElement.innerText = "I'm marker!";
+
+  const marker = new YMapMarker(
+    {
+        source: 'markerSource',
+        coordinates: [53.885437, 30.280879],
+        draggable: true,
+        mapFollowsOnDrag: true
+    },
+    markerElement
+  );
+
+  // Добавляем слой для отображения схематической карты
+  map.addChild(new YMapDefaultSchemeLayer());
+  
+  map.addChild(marker);
 }
