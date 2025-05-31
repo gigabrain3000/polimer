bd6c9be93189afce8159a91a12dfec67858a8124
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2025-05-31 22:08:05 +0300

    style: stylize popup

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ export function generatePopUp(e) {
     Swal.fire({
       title: '<strong>Свяжитесь с нами – это просто!</strong>',
       text: 'Хотите узнать больше или обсудить сотрудничество? Мы всегда на связи! Гарантируем быструю обратную связь и профессиональный подход. Ваши вопросы – наша возможность предложить лучшее решение!',
+      color: "#000000",
       imageUrl: "./assets/images/customer-support.jpg",
       width: "70%",
       imageWidth: "70%",
@@ -12,8 +13,10 @@ export function generatePopUp(e) {
       cancelButtonText: '<a href="mailto:a741006a@mail.ru" class="link link_popup">Написать</a>',
       buttonsStyling: false,
       customClass: {
+        popup: 'custom__popup',
         confirmButton: 'button button_popup',
-        cancelButton: 'button button_popup'
+        cancelButton: 'button button_popup',
+        image: "popup_img",
       }
     });
   }
