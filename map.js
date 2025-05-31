a00606eb33837d45cb4970e3443199c6ba7cbafe
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2025-05-31 20:44:48 +0300

    feat: create pop

--- map.js
+++ map.js
@@ -19,34 +19,4 @@ export async function initMap() {
   // Добавляем слой карты
   map.addChild(new YMapDefaultSchemeLayer());
   map.addChild(new YMapDefaultFeaturesLayer());
-
-  // Добавляем маркер
-  // const marker = new YMapMarker({
-  //   coordinates: [30.28070, 53.88556], // Координаты маркера
-  //   draggable: false, // Маркер статичен
-  //   icon: '<div style="width: 20px; height: 20px; background-color: red; border-radius: 50%;"></div>' // Простая стилизация маркера
-  // });
-
-  // map.addChild(marker);
-//   ymaps3.import.registerCdn('https://cdn.jsdelivr.net/npm/@yandex/ymaps3-default-ui-theme', [
-//   '@yandex/ymaps3-default-ui-theme@0.0.19'
-// ]);
-
-// после этого можем импортировать компоненты из пакета в проект
-// const {YMapDefaultMarker} = await ymaps3.import('@yandex/ymaps3-default-ui-theme');
-// map.addChild(
-//   new YMapDefaultMarker({
-//     coordinates: [30.28070, 53.88556],
-//     title: 'Полимерснабжение'
-//   })
-// );
-
-const controls = new YMapControls();
-controls.addChild(
-  new YMapZoomControl({
-    easing: 'linear'
-  })
-);
-
-map.addChild(controls);
 }
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { initMap } from "./map.js";
+import { generatePopUp } from "./popup.js";
 import { slider } from "./slider.js";
 
 document.addEventListener("DOMContentLoaded", slider);
+document.querySelectorAll(".button").forEach(item => item.addEventListener("click", generatePopUp));
 initMap();
